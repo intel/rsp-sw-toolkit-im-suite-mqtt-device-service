63a8383e8ca0650a28d63501977fadbcb7bc46de
alexander-j-saites <alexander.j.saites@intel.com>
2019-10-29 16:21:10 -0700

    Add meta-schemas test; fix invalid schemas update expect to v1.1.2

---
+++ cmd/res/schemas/schemas_test.go
@@ -0,0 +1,62 @@
+/* Apache v2 license
+*  Copyright (C) <2019> Intel Corporation
+*
+*  SPDX-License-Identifier: Apache-2.0
+ */
+
+package main
+
+import (
+	"github.impcloud.net/RSP-Inventory-Suite/expect"
+	"github.impcloud.net/RSP-Inventory-Suite/gojsonschema"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func testSchemasDir(t *testing.T, dir string) {
+	w := expect.WrapT(t)
+	schemaData := w.ShouldHaveResult(ioutil.ReadFile(dir + "_meta_schema.json")).([]byte)
+	metaSchema := w.Asf("create schema from %q", dir).ShouldHaveResult(
+		gojsonschema.NewSchema(gojsonschema.NewBytesLoader(schemaData))).
+	(*gojsonschema.Schema)
+
+	w.As("walk " + dir).ShouldSucceed(filepath.Walk(dir,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				if path != dir {
+					return filepath.SkipDir
+				} else {
+					return nil
+				}
+			}
+
+			t.Run(info.Name(), func(tt *testing.T) {
+				dw := expect.WrapT(tt).Asf("%q", path)
+				if !strings.HasSuffix(info.Name(), "_schema.json") {
+					dw.Errorf("file %q does not end in _schema.json", path)
+					return
+				}
+				content := dw.StopOnMismatch().ShouldHaveResult(ioutil.ReadFile(path)).([]byte)
+				r := dw.ShouldHaveResult(metaSchema.Validate(
+					gojsonschema.NewBytesLoader(content))).(*gojsonschema.Result)
+				dw.ShouldBeEmpty(r.Errors())
+			})
+
+			return nil
+		}))
+}
+
+func TestSchemas(t *testing.T) {
+	t.Run("test incoming schemas", func(t *testing.T) {
+		testSchemasDir(t, "incoming")
+	})
+	t.Run("test responses schemas", func(t *testing.T) {
+		testSchemasDir(t, "responses")
+	})
+}
--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/gorilla/mux v1.7.0 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pkg/errors v0.8.1
-	github.impcloud.net/RSP-Inventory-Suite/expect v1.1.1
+	github.impcloud.net/RSP-Inventory-Suite/expect v1.1.2
 	github.impcloud.net/RSP-Inventory-Suite/gojsonschema v1.1.0
 	github.impcloud.net/RSP-Inventory-Suite/tagcode v1.2.0
 	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd // indirect
